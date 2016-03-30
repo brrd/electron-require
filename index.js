fb9868e36278793625256dc32276be6d47098f10
brrd <contact@brrd.fr>
2016-03-30 15:21:25 +0200

    Customizable methods

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 "use strict";
 
+const electron = require("electron");
 const path = require("path");
+const app = process.type === "browser" ? electron.app : electron.remote.app;
+const appRelPath = path.relative(__dirname, app.getAppPath());
 
 let rq = (module) => require.main.require(module);
 
-rq.root = (module) => rq(path.join("../../", module));
-
-rq.renderer = (module) => rq(path.join("../renderer/", module));
-
-rq.main = (module) => rq(path.join("../main/", module));
-rq.browser = rq.main;
-
-rq.remote = (module) => {
+let remote = (module) => {
     if (process.type === "renderer") {
         return require("electron").remote.require(module);
     } else {
-        // fallback on rq.main if main process
-        return rq.main(module);
+        // fallback on rq if process isnt renderer
+        return rq(module);
     }
 };
 
-rq.set = (key, root) => {
-    rq[key] = (module) => require(path.join(root, module));
+let set = (arg1, arg2) => {
+    if (arg1 == null) throw Error("Undefined key is not allowed in rq.set()");
+    // Supports multiple declaration: [{ key, path }, { key, path }]
+    if (typeof arg1 === "object") {
+        for (let key in arg1) {
+            rq.set(key, arg1[key]);
+        }
+    } else {
+        let key = arg1;
+        let dir = arg2;
+        if (!path.isAbsolute(dir)) {
+            dir = path.join(appRelPath, dir);
+        }
+        rq[key] = (module) => require(path.join(dir, module));
+    }
 };
+
+// Immutable properties
+Object.defineProperties(rq, {
+    "remote": { value: remote },
+    "set": { value: set }
+});
+
+// Set default config
+rq.set({
+    "root": "",
+    "renderer": "app/renderer",
+    "main": "app/main",
+    "browser": "app/main"
+});
 
 module.exports = rq;
